b26bd698fe1c886954364a72f57d79936293e0d9
Nolan Nguyen! <nolan2087@gmail.com>
2024-04-02 11:15:24 -0700

    adding orthographic matrix

---
+++ src/matrix-library/orthographicMatrix.js
@@ -0,0 +1,32 @@
+import { FiddleMatrix } from "./matrix"
+
+class OrthoMatrix extends FiddleMatrix {
+    constructor(flatDim){
+        //Start with the identity matrix
+        super()
+
+        /* Depending on what dimension we want to "flatten" or get rid of, 
+        we deal with a different row. */
+
+        switch (flatDim) {
+            case "x":
+                flatDim = 0
+                break
+            case "y":
+                flatDim = 1
+                break
+            case "z":
+                flatDim = 2
+                break
+        }
+        
+        this.matrix[flatDim][0] = 0
+        this.matrix[flatDim][1] = 0
+        this.matrix[flatDim][2] = 0
+
+        //Re-set rows and columns so that the scalar values show up in those attributes - IMPORTANT FOR MATRIX OPERATIONS
+        this.updateRC()
+    }
+}
+
+export default OrthoMatrix
