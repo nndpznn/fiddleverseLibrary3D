b88ed545aa753dae65981be003d7a9fd89381d89
JD-Elia <jelia@lion.lmu.edu>
2024-04-17 16:37:41 -0700

    I FINALLY FIXED THE STAR I'M THE GREATEST CODER KNOWN TO MAN

--- src/fiddleverse/star.js
+++ src/fiddleverse/star.js
@@ -18,7 +18,9 @@ class StarShape extends fiddle3D {
         [POSITION.x + SIZE, POSITION.y, POSITION.z], // 8 (Right center)
         [POSITION.x + SIZE * 2.5, POSITION.y + SIZE * 0.5, POSITION.z], // 9 (Top right outer)
         [POSITION.x, POSITION.y + SIZE * 2, POSITION.z], // 10 (Top center)
-        [POSITION.x, POSITION.y + SIZE * 1.5, POSITION.z - SIZE * 0.25] // 11 (Top center, slightly back)
+        [POSITION.x, POSITION.y + SIZE * 1.5, POSITION.z - SIZE * 0.25], // 11 (Top center, slightly back)
+        [POSITION.x + SIZE, POSITION.y + SIZE, POSITION.z] // 12 (fixed vertice for 10)
+        //try another point becuase 10 is not placed
       ],
       [
         [0, 1, 2],
@@ -29,7 +31,7 @@ class StarShape extends fiddle3D {
         [0, 6, 7],
         [0, 7, 8],
         [0, 8, 9],
-        [0, 9, 10],
+        // [0, 9, 10],
         [0, 10, 1], 
         [11, 1, 10],
         [11, 2, 1],
@@ -40,15 +42,31 @@ class StarShape extends fiddle3D {
         [11, 7, 6],
         [11, 8, 7],
         [11, 9, 8],
-        [11, 10, 9],
+        // [11, 10, 9],
         [0, 10, 1],
-        [0, 9, 10],
+        [0, 9, 12],
+        [0, 12, 10],
         [0, 8, 9],
         [0, 7, 8],
-        [0, 6, 7]
+        [0, 6, 7],
+        
       ],
       color
     );
+
+    this.normals = (
+      0.0, 1.0, 0.0,
+      0.0, 1.0, 0.0,
+      0.0, 1.0, 0.0, 
+
+      0.0, 1.0, 0.0, 
+      0.0, 1.0, 0.0, 
+      0.0, 1.0, 0.0, 
+
+      0.0, 1.0, 0.0, 
+      0.0, 1.0, 0.0
+
+    )
   }
 }
 
