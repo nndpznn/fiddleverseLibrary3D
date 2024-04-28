8f086fcffed924a68ad18531bc14639bdf35a660
AllenBoyce <allenboyceiii@gmail.com>
2024-04-27 20:15:12 -0700

    Added UFO

--- src/testPages/PitchedScene.js
+++ src/testPages/PitchedScene.js
@@ -14,8 +14,7 @@ import { StarShape } from '../fiddleverse/star'
 import FiddlewingThing from '../fiddleverse/FiddlewingThing'
 import IcosphereThing from '../fiddleverse/IcosphereThing'
 import { pyramidShape } from '../fiddleverse/pyramid'
-
-
+import ScaleMatrix from '../matrix-library/scaleMatrix'
 
 const CANVAS_WIDTH = 1024
 const CANVAS_HEIGHT = 512
@@ -91,209 +90,224 @@ const PitchedScene = props => {
       return
     }
 
- // Grab the WebGL rendering context.
- const fiddleverse = new Fiddleverse(canvas, screenHeight, screenWidth, VERTEX_SHADER, FRAGMENT_SHADER)
- const gl = fiddleverse.gl
+    // Grab the WebGL rendering context.
+    const fiddleverse = new Fiddleverse(canvas, screenHeight, screenWidth, VERTEX_SHADER, FRAGMENT_SHADER)
+    const gl = fiddleverse.gl
 
- const blueColor = {r: 0.18, g: 0.62, b: 0.82}
- const grayColor = {r: 0.3,  g: 0.3,  b: 0.3}
- const yellowColor = {r: 0.95, g: 0.73, b: 0.05}
+    const blueColor = { r: 0.18, g: 0.62, b: 0.82 }
+    const grayColor = { r: 0.3, g: 0.3, b: 0.3 }
+    const yellowColor = { r: 0.95, g: 0.73, b: 0.05 }
 
- const cubeTest = new cubeShape(gl, blueColor, 0.5, {x: 0, y: 0, z: 0})
- cubeTest.wireframe = false
- cubeTest.smooth = false
+    const cubeTest = new cubeShape(gl, grayColor, 0.5, { x: 0, y: 0, z: 0 })
+    cubeTest.wireframe = false
+    cubeTest.smooth = false
 
- const octocylinder1 = new octocylinderShape(gl, blueColor)
- octocylinder1.wireframe = false
- octocylinder1.smooth = false
- const rotateOctoX = new RotationMatrix(90, 1, 0, 0)
- octocylinder1.setInstanceTransformation(rotateOctoX)
+    const octocylinder1 = new octocylinderShape(gl, blueColor)
+    octocylinder1.wireframe = false
+    octocylinder1.smooth = false
+    const rotateOctoX = new RotationMatrix(90, 1, 0, 0)
+    octocylinder1.setInstanceTransformation(rotateOctoX)
 
- const octocylinderOutline = new octocylinderShape(gl, grayColor)
- octocylinderOutline.wireframe = true
+    const octocylinderOutline = new octocylinderShape(gl, grayColor)
+    octocylinderOutline.wireframe = true
 
- const starTest = new StarShape(gl, 0.65, yellowColor, {x: 0, y: 0, z: 0})
- starTest.wireframe = false
- starTest.smooth = false
- const rotateStarY = new RotationMatrix(90, 1, 0, 0)
+    const starTest = new StarShape(gl, 0.65, yellowColor, { x: 0, y: 0, z: 0 })
+    starTest.wireframe = false
+    starTest.smooth = false
+    const rotateStarY = new RotationMatrix(90, 1, 0, 0)
 
- const sphereTest = new IcosphereThing(gl, yellowColor)
- sphereTest.wireframe = false
- sphereTest.smooth = true
+    const sphereTest = new IcosphereThing(gl, yellowColor)
+    sphereTest.wireframe = false
+    sphereTest.smooth = true
 
- const pyramidTest = new pyramidShape(gl, blueColor)
- pyramidTest.wireframe = false
- pyramidTest.smooth = false
- 
+    const pyramid1Test = new pyramidShape(gl, grayColor)
+    pyramid1Test.wireframe = false
+    pyramid1Test.smooth = false
 
- const wing1 = new FiddlewingThing(gl)
- wing1.wireframe = false
- const rotateWingY = new RotationMatrix(90, 0, 1, 0)
- const rotateWingZ = new RotationMatrix(90, 0, 0, 1)
- const translateWing = new TranslationMatrix(-0.5, 0, 0)
- wing1.setInstanceTransformation(rotateWingY)
- wing1.setInstanceTransformation(rotateWingZ)
- wing1.setInstanceTransformation(translateWing)
+    const pyramid2Test = new pyramidShape(gl, grayColor)
+    pyramid2Test.wireframe = false
+    pyramid2Test.smooth = false
 
- const wing2 = new FiddlewingThing(gl)
- wing2.wireframe = false
- const rotateWing2Y = new RotationMatrix(-90, 0, 1, 0)
- const rotateWing2Z = new RotationMatrix(-90, 0, 0, 1)
- const translateWing2 = new TranslationMatrix(0.5, 0, 0)
- wing2.setInstanceTransformation(rotateWing2Y)
- wing2.setInstanceTransformation(rotateWing2Z)
- wing2.setInstanceTransformation(translateWing2)
+    const wing1 = new FiddlewingThing(gl)
+    wing1.wireframe = false
+    const rotateWingY = new RotationMatrix(90, 0, 1, 0)
+    const rotateWingZ = new RotationMatrix(90, 0, 0, 1)
+    const translateWing = new TranslationMatrix(-0.5, 0, 0)
+    wing1.setInstanceTransformation(rotateWingY)
+    wing1.setInstanceTransformation(rotateWingZ)
+    wing1.setInstanceTransformation(translateWing)
 
- octocylinder1.add(wing1)
- octocylinder1.add(wing2)
- 
+    const wing2 = new FiddlewingThing(gl)
+    wing2.wireframe = false
+    const rotateWing2Y = new RotationMatrix(-90, 0, 1, 0)
+    const rotateWing2Z = new RotationMatrix(-90, 0, 0, 1)
+    const translateWing2 = new TranslationMatrix(0.5, 0, 0)
+    wing2.setInstanceTransformation(rotateWing2Y)
+    wing2.setInstanceTransformation(rotateWing2Z)
+    wing2.setInstanceTransformation(translateWing2)
 
- const moveLeft = new TranslationMatrix(-2.3, 0, 0)
- const moveRight = new TranslationMatrix(2, 0, 0)
- const moveUp = new TranslationMatrix(0, 1, 0)
- const moveDown = new TranslationMatrix(0, -1.3, 0)
- const setMiddle = new TranslationMatrix(0, 0, 0)
- cubeTest.setInstanceTransformation(moveRight)
- starTest.setInstanceTransformation(moveLeft)
- sphereTest.setInstanceTransformation(moveLeft)
- pyramidTest.setInstanceTransformation(moveDown)
+    octocylinder1.add(wing1)
+    octocylinder1.add(wing2)
 
- sphereTest.add(starTest) 
+    const moveLeft = new TranslationMatrix(-2.3, 0, 0)
+    const moveRight = new TranslationMatrix(2, 0, 0)
+    const moveUp = new TranslationMatrix(0, 1, 0)
+    const moveDown = new TranslationMatrix(0, -1.3, 0)
+    const setMiddle = new TranslationMatrix(0, 0, 0)
 
- // Pass the vertices to WebGL.
- fiddleverse.add(octocylinder1)
- fiddleverse.add(cubeTest)
- fiddleverse.add(starTest)
- fiddleverse.add(sphereTest)
- fiddleverse.add(pyramidTest)
- 
- fiddleverse.process()
- fiddleverse.light = [0, 0, -1]
- fiddleverse.cameraPosition = [0, 0, -1]
- fiddleverse.cameraView = [0, 0, 0]
+    starTest.setInstanceTransformation(moveLeft)
+    sphereTest.setInstanceTransformation(moveLeft)
 
- /*
-  * Displays the scene.
-  */
- let currentRotation = 0.0
- fiddleverse.drawScene(currentRotation)
+    pyramid1Test.setInstanceTransformation(new RotationMatrix(90, 0, 0, 1))
+    pyramid1Test.setInstanceTransformation(new TranslationMatrix(1, 0, 0))
+    pyramid2Test.setInstanceTransformation(new RotationMatrix(-90, 0, 0, 1))
+    pyramid2Test.setInstanceTransformation(new TranslationMatrix(3, 0, 0))
+    cubeTest.setInstanceTransformation(moveRight)
 
- /*
-  * Animates the scene.
-  */
- let animationActive = false
- let previousTimestamp = null
+    cubeTest.add(pyramid1Test)
+    cubeTest.add(pyramid2Test)
 
- let cameraSwitched = true
+    const halfSize = new ScaleMatrix(0.5, 0.5, 0.5)
+    // pyramid1Test.setInstanceTransformation(halfSize)
+    // pyramid2Test.setInstanceTransformation(halfSize)
+    cubeTest.setInstanceTransformation(halfSize)
+    cubeTest.setInstanceTransformation(new TranslationMatrix(1, 0, 0))
 
- const FRAMES_PER_SECOND = 60
- const MILLISECONDS_PER_FRAME = 1000 / FRAMES_PER_SECOND
+    sphereTest.add(starTest)
 
- const DEGREES_PER_MILLISECOND = 0.5
- const FULL_CIRCLE = 360.0
+    // Pass the vertices to WebGL.
+    fiddleverse.add(octocylinder1)
+    fiddleverse.add(cubeTest)
+    fiddleverse.add(starTest)
+    fiddleverse.add(sphereTest)
+    fiddleverse.add(pyramid1Test)
+    fiddleverse.add(pyramid2Test)
 
- const advanceScene = timestamp => {
-   // Check if the user has turned things off.
-   if (!animationActive) {
-     return
-   }
+    fiddleverse.process()
+    fiddleverse.light = [0, 0, -1]
+    fiddleverse.cameraPosition = [0, 0, -1]
+    fiddleverse.cameraView = [0, 0, 0]
 
-   // Initialize the timestamp.
-   if (!previousTimestamp) {
-     previousTimestamp = timestamp
-     window.requestAnimationFrame(advanceScene)
-     return
-   }
+    /*
+     * Displays the scene.
+     */
+    let currentRotation = 0.0
+    fiddleverse.drawScene(currentRotation)
 
-   // Check if it’s time to advance.
-   const progress = timestamp - previousTimestamp
-   if (progress < MILLISECONDS_PER_FRAME) {
-     // Do nothing if it’s too soon.
-     window.requestAnimationFrame(advanceScene)
-     return
-   }
+    /*
+     * Animates the scene.
+     */
+    let animationActive = false
+    let previousTimestamp = null
 
-   // All clear.
-   currentRotation += DEGREES_PER_MILLISECOND * progress
+    let cameraSwitched = true
 
-   // fiddleverse.translationVector[0] -= 0.01
+    const FRAMES_PER_SECOND = 60
+    const MILLISECONDS_PER_FRAME = 1000 / FRAMES_PER_SECOND
 
-   const rotateX = new RotationMatrix(DEGREES_PER_MILLISECOND, 1, 0, 0)
-  //  const rotateY = new RotationMatrix(DEGREES_PER_MILLISECOND, 0, 1, 0)
-  //  const rotateZ = new RotationMatrix(DEGREES_PER_MILLISECOND, 0, 0, 1)
+    const DEGREES_PER_MILLISECOND = 0.5
+    const FULL_CIRCLE = 360.0
 
-   octocylinder1.setInstanceTransformation(rotateX)
-  //  octocylinder1.setInstanceTransformation(rotateOctoY)
-  //  octocylinder1.setInstanceTransformation(rotateOctoZ)
+    const advanceScene = timestamp => {
+      // Check if the user has turned things off.
+      if (!animationActive) {
+        return
+      }
 
-   if (cubeMoving) {
-    cubeTest.setInstanceTransformation(rotateX)
-   }
+      // Initialize the timestamp.
+      if (!previousTimestamp) {
+        previousTimestamp = timestamp
+        window.requestAnimationFrame(advanceScene)
+        return
+      }
 
-  //  const moveMatrix = new TranslationMatrix(0, 0, -1)
+      // Check if it’s time to advance.
+      const progress = timestamp - previousTimestamp
+      if (progress < MILLISECONDS_PER_FRAME) {
+        // Do nothing if it’s too soon.
+        window.requestAnimationFrame(advanceScene)
+        return
+      }
 
-   if (fiddleverse.translationVector[0] < -0.5) {
-     fiddleverse.translationVector[0] = 1
-   }
+      // All clear.
+      currentRotation += DEGREES_PER_MILLISECOND * progress
 
-   if (fiddleverse.translationVector[1] < -.9) {
-     fiddleverse.translationVector[1] = 1
-   }
+      // fiddleverse.translationVector[0] -= 0.01
 
-   if (currentRotation >= FULL_CIRCLE) {
-     currentRotation -= FULL_CIRCLE
-   }
+      const rotateX = new RotationMatrix(DEGREES_PER_MILLISECOND, 1, 0, 0)
+      //  const rotateY = new RotationMatrix(DEGREES_PER_MILLISECOND, 0, 1, 0)
+      //  const rotateZ = new RotationMatrix(DEGREES_PER_MILLISECOND, 0, 0, 1)
 
-   fiddleverse.drawScene(currentRotation)
+      octocylinder1.setInstanceTransformation(rotateX)
+      //  octocylinder1.setInstanceTransformation(rotateOctoY)
+      //  octocylinder1.setInstanceTransformation(rotateOctoZ)
 
-   // Request the next frame.
-   previousTimestamp = timestamp
-   window.requestAnimationFrame(advanceScene)
- }
+      if (cubeMoving) {
+        cubeTest.setInstanceTransformation(rotateX)
+      }
 
- // Draw the initial scene.
- fiddleverse.drawScene(currentRotation)
+      //  const moveMatrix = new TranslationMatrix(0, 0, -1)
 
- setFiddleverse({
-   toggleRotation: () => {
-     animationActive = !animationActive
-     if (animationActive) {
-       previousTimestamp = null
-       window.requestAnimationFrame(advanceScene)
-     }
-   },
+      if (fiddleverse.translationVector[0] < -0.5) {
+        fiddleverse.translationVector[0] = 1
+      }
 
-   switchCamera: () => {
-     console.log("Switching camera")
-     cameraSwitched = !cameraSwitched
-     //Toggle cameraPosition to demonstrate camera controls
-     if(cameraSwitched){
-       fiddleverse.cameraPosition = [0, 0, -1]
-     }
-     else{
-       fiddleverse.cameraPosition = [0, -1, -1]
-     }
-     
-     //Redraw scene so that we can see the change
-     fiddleverse.drawScene(currentRotation)
-   },
+      if (fiddleverse.translationVector[1] < -0.9) {
+        fiddleverse.translationVector[1] = 1
+      }
 
-   startCube: () => {
-      cubeMoving = !cubeMoving     //look here for cube when searching
-   },
+      if (currentRotation >= FULL_CIRCLE) {
+        currentRotation -= FULL_CIRCLE
+      }
 
-   removeSomething: () => {
-    if (octocylinder1.present) {
-      fiddleverse.remove(octocylinder1)
       fiddleverse.drawScene(currentRotation)
-    } else {
-      fiddleverse.add(octocylinder1)
-      fiddleverse.drawScene(currentRotation)
+
+      // Request the next frame.
+      previousTimestamp = timestamp
+      window.requestAnimationFrame(advanceScene)
     }
-   }
 
- })
-}, [canvasRef])
+    // Draw the initial scene.
+    fiddleverse.drawScene(currentRotation)
+
+    setFiddleverse({
+      toggleRotation: () => {
+        animationActive = !animationActive
+        if (animationActive) {
+          previousTimestamp = null
+          window.requestAnimationFrame(advanceScene)
+        }
+      },
+
+      switchCamera: () => {
+        console.log('Switching camera')
+        cameraSwitched = !cameraSwitched
+        //Toggle cameraPosition to demonstrate camera controls
+        if (cameraSwitched) {
+          fiddleverse.cameraPosition = [0, 0, -1]
+        } else {
+          fiddleverse.cameraPosition = [0, -1, -1]
+        }
+
+        //Redraw scene so that we can see the change
+        fiddleverse.drawScene(currentRotation)
+      },
+
+      startCube: () => {
+        cubeMoving = !cubeMoving //look here for cube when searching
+      },
+
+      removeSomething: () => {
+        if (octocylinder1.present) {
+          fiddleverse.remove(octocylinder1)
+          fiddleverse.drawScene(currentRotation)
+        } else {
+          fiddleverse.add(octocylinder1)
+          fiddleverse.drawScene(currentRotation)
+        }
+      }
+    })
+  }, [canvasRef])
 
   // Set up the rotation toggle: clicking on the canvas does it.
   const handleStartStop = event => fiddleverse.toggleRotation()
@@ -317,29 +331,28 @@ const PitchedScene = props => {
       </canvas>
 
       <button disabled={!fiddleverse} onClick={handleStartStop}>
-            Master Start/Stop
+        Master Start/Stop
       </button>
 
       <button disabled={!fiddleverse} onClick={handleSwitchCamera}>
-            Switch Camera
+        Switch Camera
       </button>
 
       <button disabled={!fiddleverse} onClick={handleStartCube}>
-            Start/Stop Cube
+        Start/Stop Cube
       </button>
 
       <button disabled={!fiddleverse} onClick={handleRemove}>
-            Add/Remove Satellite
+        Add/Remove Satellite
       </button>
 
       <button disabled={!fiddleverse} onClick={handleStartSun}>
-            Start/Stop Sun
+        Start/Stop Sun
       </button>
 
       <button disabled={!fiddleverse} onClick={handleStartPyramid}>
-            Start/Stop Pyramid
+        Start/Stop Pyramid
       </button>
-
     </article>
   )
 }
